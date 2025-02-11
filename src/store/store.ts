d8e27bb3b15c2137c7acf1bb10bd2dec7fbf43da
Mahmoud Abdelfatah <mahmoud.dev97@gmail.com>
2025-02-11 08:49:23 +0200

    refactor: improve code formatting and readability in ExpenseList component and store configuration

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,21 +1,21 @@
-import { configureStore, combineReducers } from "@reduxjs/toolkit"
-import { persistStore, persistReducer } from "redux-persist"
-import storage from "redux-persist/lib/storage"
-import expenseReducer from "./expenseSlice"
-import themeReducer from "./themeSlice"
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import expenseReducer from "./expenseSlice";
+import themeReducer from "./themeSlice";
 
 const persistConfig = {
   key: "root",
   storage,
-  whitelist: ["expenses", "theme"], // persist both expenses and theme
-}
+  whitelist: ["expenses", "theme"],
+};
 
 const rootReducer = combineReducers({
   expenses: expenseReducer,
   theme: themeReducer,
-})
+});
 
-const persistedReducer = persistReducer(persistConfig, rootReducer)
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
   reducer: persistedReducer,
@@ -23,10 +23,9 @@ export const store = configureStore({
     getDefaultMiddleware({
       serializableCheck: false,
     }),
-})
+});
 
-export const persistor = persistStore(store)
+export const persistor = persistStore(store);
 
-export type RootState = ReturnType<typeof store.getState>
-export type AppDispatch = typeof store.dispatch
-
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
